b32c23ed197549fb2dd2d440767250e5eaadb66b
Sara Contreras <smct0001@ce.pucmm.edu.do>
2023-05-14 17:57:24 -0400

    Continuando trabajando

--- src/main/java/edu/pucmm/pw/Main.java
+++ src/main/java/edu/pucmm/pw/Main.java
@@ -1,5 +1,11 @@
 package edu.pucmm.pw;
 
+import com.sun.net.httpserver.Headers;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
 import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URL;
@@ -11,25 +17,49 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Ingrese una URL: ");
-        try{
-            //Obtenemos y validamos la URL
-            String urlValidacion =  scanner.nextLine();
-            URL url = new URL(urlValidacion);
+        boolean urlValida = false;
+        while (!urlValida) {
+            try {
+                System.out.print("Ingrese una URL: ");
+                String urlValidacion = scanner.nextLine();
+                URL url = new URL(urlValidacion);
+                urlValida = true;
+
+                HttpClient cliente = HttpClient.newHttpClient();
+                HttpRequest solicitud = HttpRequest.newBuilder(URI.create(String.valueOf(url))).build();
+                HttpResponse respuesta = cliente.send(solicitud, HttpResponse.BodyHandlers.discarding());
+
+                String tipoRecurso = respuesta.headers().firstValue("Content-Type").orElse("Tipo del recurso no encontrado");
+                System.out.println("Tipo de recurso: " + tipoRecurso);
+
+                if(tipoRecurso.contains("text/html")){
+                    //1
+                    Document doc = Jsoup.connect(String.valueOf(url)).get();
+                    String html = doc.html();
+                    int cantLineas = html.split("\n").length;
+                    System.out.println("Cantidad de lineas: " + cantLineas);
+                   //2
+                    Elements parrafo = doc.select("p");
+                    int cantParr = parrafo.size();
+                    System.out.println("Cantidad de parrafos: " + cantParr);
+                    //3
+                    Elements img = doc.select("img");
+                    int cantImg = img.size();
+                    System.out.println("Cantidad de imagenes: " + cantImg);
 
 
-            HttpClient cliente = HttpClient.newHttpClient();
-            //Solicitud a pagina web
-            HttpRequest solicitud = HttpRequest.newBuilder(URI.create(String.valueOf(url))).build();//ENTRE COMILLAS VA LA URL QUE QUEREMOS ACCEDER
 
-            HttpResponse respuesta = cliente.send(solicitud, HttpResponse.BodyHandlers.ofString());
 
-            //System.out.println(respuesta.body());
 
-        } catch (MalformedURLException e){
-        System.out.println("URL no valida. Ingrese otra URL");
-        } catch (Exception e){
-            e.printStackTrace();
+
+                }
+
+            } catch (MalformedURLException e) {
+                System.out.println("URL no valida. Ingrese otra URL");
+                urlValida = false;
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
     }
 }
