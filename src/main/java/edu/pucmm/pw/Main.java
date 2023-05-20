ddd179d3610783dff4559584548f5bb54893a8c5
Sara Contreras <smct0001@ce.pucmm.edu.do>
2023-05-20 12:27:07 -0400

    Continuando trabajando

--- src/main/java/edu/pucmm/pw/Main.java
+++ src/main/java/edu/pucmm/pw/Main.java
@@ -14,6 +14,7 @@ import java.net.http.HttpResponse;
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         boolean urlValida = false;
