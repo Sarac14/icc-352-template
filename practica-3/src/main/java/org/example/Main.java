0e156e7f5a7a5b467a32adddf5bd0c6c58fb8fd5
Sara Contreras <smct0001@ce.pucmm.edu.do>
2023-06-14 18:10:20 -0400

    Probando

--- practica-3/src/main/java/org/example/Main.java
+++ practica-3/src/main/java/org/example/Main.java
@@ -2,6 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println("PROBANDO 1 2 1 2 PROBANDO GLORIA A DIOS");
     }
 }
