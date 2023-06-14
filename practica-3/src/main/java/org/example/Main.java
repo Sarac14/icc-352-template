4ab3f31001f9a62d6080b5462b9dec547eb4a2cb
Higinio01 <hrsp0001@ce.pucmm.edu.do>
2023-06-14 18:19:53 -0400

    Probando x2

--- practica-3/src/main/java/org/example/Main.java
+++ practica-3/src/main/java/org/example/Main.java
@@ -4,5 +4,8 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("PROBANDO 1 2 1 2 PROBANDO GLORIA A DIOS");
+        System.out.println("VIVA DIO QUE SE MUERA EI DIABLO");
+
+
     }
 }
