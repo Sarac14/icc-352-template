06b44916b6ee539d3917747c9b3419bb4aa73ed3
Sara Contreras <smct0001@ce.pucmm.edu.do>
2023-06-17 09:38:24 -0400

    Main

--- practica-3/src/main/java/org/example/Main.java
+++ practica-3/src/main/java/org/example/Main.java
@@ -1,9 +1,53 @@
 package org.example;
 
+import io.javalin.Javalin;
+import io.javalin.http.staticfiles.Location;
+import io.javalin.plugin.bundled.RouteOverviewPlugin;
+import org.example.servicios.BootStrapServices;
+
 public class Main {
+    private static String modoConexion = "";
+
     public static void main(String[] args) {
 
-        System.out.println("PROBANDO 1 2 1 2 PROBANDO GLORIA A DIOS");
-        System.out.println("VIVA DIO QUE SE MUERA EL DiiihhiiIABLO");
+        if(args.length >= 1){
+            modoConexion = args[0];
+            //System.out.println("Modo de Operacion: "+modoConexion);
+        }
+
+        if(modoConexion.isEmpty()) {
+            BootStrapServices.getInstancia().init();
+        }
+
+        Javalin app = Javalin.create(config ->{
+
+            config.staticFiles.add(staticFileConfig -> {
+                staticFileConfig.hostedPath = "/";
+                staticFileConfig.directory = "/publico";
+                staticFileConfig.location = Location.CLASSPATH;
+            }); //desde la carpeta de resources
+
+            config.plugins.register(new RouteOverviewPlugin("/rutas")); //aplicando plugins de las rutas
+            config.plugins.enableCors(corsContainer -> {
+                corsContainer.add(corsPluginConfig -> {
+                    corsPluginConfig.anyHost();
+                });
+            });
+        }).start(getHerokuAssignedPort());
+
     }
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 7000; //Retorna el puerto por defecto en caso de no estar en Heroku.
+    }
+
+    public static String getModoConexion(){
+        return modoConexion;
+    }
+
+
+
 }
