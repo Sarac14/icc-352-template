596865cffed36faf41ef329d795c400bf4fd956b
Sara Contreras <smct0001@ce.pucmm.edu.do>
2023-07-05 16:45:50 -0400

    Corrigiendo unos errores en la practica 1

--- practica-1/practica-1/src/main/java/edu/pucmm/pw/Main.java
+++ practica-1/practica-1/src/main/java/edu/pucmm/pw/Main.java
@@ -16,6 +16,7 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
         try {
             System.out.print("Ingrese una URL: ");
