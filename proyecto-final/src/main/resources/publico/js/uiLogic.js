cc6306c96cc91afefcc30672bbf9fa754e07470d
Sara Contreras <smct0001@ce.pucmm.edu.do>
2023-08-22 22:44:51 -0400

    Arreglandno problema de la imagen base64

--- proyecto-final/src/main/resources/publico/js/uiLogic.js
+++ proyecto-final/src/main/resources/publico/js/uiLogic.js
@@ -180,6 +180,39 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 
 
+function compressImage(srcData, maxWidth, maxHeight, callback) {
+    const img = new Image();
+    img.src = srcData;
+
+    img.onload = function() {
+        let width = img.width;
+        let height = img.height;
+
+        // Mantener la relación de aspecto
+        if (width > height) {
+            if (width > maxWidth) {
+                height *= maxWidth / width;
+                width = maxWidth;
+            }
+        } else {
+            if (height > maxHeight) {
+                width *= maxHeight / height;
+                height = maxHeight;
+            }
+        }
+
+        const canvas = document.createElement("canvas");
+        canvas.width = width;
+        canvas.height = height;
+
+        const ctx = canvas.getContext("2d");
+        ctx.drawImage(img, 0, 0, width, height);
+
+        const newDataUrl = canvas.toDataURL("image/jpeg", 0.7);
+        callback(newDataUrl);
+    };
+}
+
 
 //--------------------------WEB WORKER-------------------------------------------------
 let worker = new Worker('js/webWorker.js');
