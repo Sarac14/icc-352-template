d9abed458202ac03a75b6cdc39942a36a9aa3616
Sara Contreras <smct0001@ce.pucmm.edu.do>
2023-08-27 15:55:30 -0400

    Crear agente offline

--- proyecto-final/src/main/resources/publico/js/serviceworkers.js
+++ proyecto-final/src/main/resources/publico/js/serviceworkers.js
@@ -1,14 +1,15 @@
 /**
  * Ejemplo de un script de service workers.
  */
-const CACHE_VERSION = 1;
+/*const CACHE_VERSION = 1;
 const CURRENT_CACHES = {
     font: `font-cache-v${CACHE_VERSION}`,
-};
+};*/
 // Cache activo.
 const CACHE_NAME = 'mi-app-cache-v1';
 const urlsToCache = [
-    '/localStorage.html',
+    '/crud-simple/listar',
+    '/crud-simple/crear',
     '/uiLogic.js',
     '/visualizar.html',
     '/webWorkers.js',
@@ -16,37 +17,53 @@ const urlsToCache = [
     '/vermapa.html',
     '/login.html',
     '/serviceWorkers.js',
+    '/templates/crud-tradicional/listar.html',
+    '/templates/crud-tradicional/crearEditarVisualizar.html',
     '/index1.html'
+
 ];
 const fallback = "/html5/offline.html";
 
 self.addEventListener('install', function(event) {
-    console.log('Instalando el Service Worker');
+    console.log('Instalando el Services Worker');
+    // Utilizando las promesas para agregar los elementos definidos
     event.waitUntil(
-        caches.open(CACHE_NAME)
+        caches.open(CACHE_NAME) //Utilizando el api Cache definido en la variable.
             .then(function(cache) {
                 console.log('Cache abierto');
-                return cache.addAll(urlsToCache);
+                return cache.addAll(urlsToCache); //agregando todos los elementos del cache.
             })
     );
 });
 
-self.addEventListener('activate', event => {
-    console.log('Activando el Service Worker - Limpiando el cache no utilizado');
-    const expectedCacheNamesSet = new Set(Object.values(CURRENT_CACHES));
-    event.waitUntil(
-        caches.keys().then(cacheNames => Promise.all(
-            cacheNames.map(cacheName => {
-                if (!expectedCacheNamesSet.has(cacheName)) {
-                    console.log('Eliminando caché obsoleta:', cacheName);
-                    return caches.delete(cacheName);
+self.addEventListener('activate', evt => {
+    console.log('Activando el services worker -  Limpiando el cache no utilizado');
+    evt.waitUntil(
+        caches.keys().then(function(keyList) { //Recupera todos los cache activos.
+            return Promise.all(keyList.map(function(key) {
+                if (CACHE_NAME.indexOf(key) === -1) { //si no es el cache por defecto borramos los elementos.
+                    return caches.delete(key); //borramos los elementos guardados.
                 }
-            })
-        ))
+            }));
+        })
     );
 });
 
 self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(response=>{
+            console.log(event);
+            //si existe retornamos la petición desde el cache, de lo contrario (retorna undefined) dejamos pasar la solicitud al servidor,
+            // lo hacemos con la función fetch pasando un objeto de petición.
+            return response || fetch(event.request);
+        }).catch(function (){ //En caso de tener un problema con la red, se mostrará el caso
+            return caches.match(fallback);
+        })
+    );
+});
+
+
+/*self.addEventListener('fetch', event => {
     console.log('Manejando evento de fetch para', event.request.url);
 
     event.respondWith(
@@ -81,4 +98,4 @@ self.addEventListener('fetch', event => {
             });
         })
     );
-});
+});*/
